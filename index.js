a2fca526ff0544e1ca7eda97b00e2d9bc1881f16
BrianFernandoJara <brianfernandojara@gmail.com>
2023-08-25 18:00:39 -0400

    sending data with fetch lab complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const { Body } = require("node-fetch");
+
 // Add your code here
+const getBody = document.getElementsByTagName("body");
+
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(resp => resp.json())
+    .then(newUser => {
+        const h1 = document.createElement("h1");
+        const getBody = document.querySelector("body");
+
+        h1.innerHTML = newUser.id;
+        getBody.appendChild(h1);
+    })
+    .catch(error => {
+        const h2 = document.createElement("h2")
+        const getBody = document.querySelector("body")
+        
+        h2.innerHTML = error.message;
+        getBody.appendChild(h2);
+    })
+
+}
